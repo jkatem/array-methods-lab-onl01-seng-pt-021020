a34bafaff653f968b7a1ab16cc658bb7461a144a
Jen Miao <jenmiao@gmail.com>
2020-02-15 21:59:26 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,7 +10,9 @@ def using_sort(array)
 end
 
 def using_reverse(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  newArr = array.reverse
+  newArr
 end
 
 def using_first(array)
